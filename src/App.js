3bdbbdee75a4e9dff977e52a53a3c48a3723ef6f
yisak5 <issac1162@gmail.com>
2023-06-01 22:54:12 +0900

    2023-06-01

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
 import { useState } from "react";
 
 function App() {
-  const [todo, setToDo] = useState("");
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
-    if (todo === "") {
+    if (toDo === "") {
       return;
     }
+    setToDos((currentArray) => [toDo, ...currentArray]);
     setToDo("");
+    console.log(toDos);
   }
   return (
     <div>
+      <h1>My To Dos ({toDos.length})</h1>
       <form onSubmit={onSubmit}>
-        <input onChange={onChange} value ={todo} type="text" placeholder="Write your to do..."/>
+        <input 
+          onChange={onChange}
+          value={toDo} 
+          type="text" 
+          placeholder="Write your to do..."
+        />
         <button>App To Do!</button>
       </form>
     </div>
